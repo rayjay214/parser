d6c0daf84ef3334e710344e8ec549e99308e485d
rayjay <526528945@qq.com>
2024-01-18 20:04:11 +0800

    增加新消息解析

---
+++ ipc/body_0x8101.go
@@ -0,0 +1,49 @@
+package protocol
+
+import (
+	_ "fmt"
+)
+
+type Body_0x8101 struct {
+	AppId string
+	Ip    string
+	Port  uint16
+}
+
+func (entity *Body_0x8101) MsgID() MsgID {
+	return Msg_0x8101
+}
+
+func (entity *Body_0x8101) Encode() ([]byte, error) {
+	writer := NewWriter()
+
+	writer.WriteString(entity.AppId, 20)
+
+	writer.WriteString(entity.Ip, 20)
+
+	writer.WriteUint16(entity.Port)
+
+	return writer.Bytes(), nil
+}
+
+func (entity *Body_0x8101) Decode(data []byte) (int, error) {
+	reader := NewReader(data)
+
+	var err error
+	entity.AppId, err = reader.ReadString(20)
+	if err != nil {
+		return 0, err
+	}
+
+	entity.Ip, err = reader.ReadString(20)
+	if err != nil {
+		return 0, err
+	}
+
+	entity.Port, err = reader.ReadUint16()
+	if err != nil {
+		return 0, err
+	}
+
+	return len(data) - reader.Len(), nil
+}
