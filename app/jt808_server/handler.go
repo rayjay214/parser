e67b2ba9f2c49786e6b8b6609fe7a606f84a586f
rayjay <526528945@qq.com>
2024-02-24 13:58:49 +0800

    基站，wifi解析

--- app/jt808_server/handler.go
+++ app/jt808_server/handler.go
@@ -1,331 +1,331 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/rayjay214/parser/common"
-	"github.com/rayjay214/parser/jt808"
-	"github.com/rayjay214/parser/jt808/extra"
-	"github.com/rayjay214/parser/server"
-	"github.com/rayjay214/parser/storage"
-	log "github.com/sirupsen/logrus"
-	"io/ioutil"
-	"strconv"
-	"time"
+    "bytes"
+    "fmt"
+    "github.com/rayjay214/parser/common"
+    "github.com/rayjay214/parser/jt808"
+    "github.com/rayjay214/parser/jt808/extra"
+    "github.com/rayjay214/parser/server"
+    "github.com/rayjay214/parser/storage"
+    log "github.com/sirupsen/logrus"
+    "io/ioutil"
+    "strconv"
+    "time"
 )
 
 func handle0100(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0100)
-	log.Infof("handle 0100 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0100)
+    log.Infof("handle 0100 %v", entity)
 
-	session.ReplyRegister(message)
+    session.ReplyRegister(message)
 }
 
 func handle0102(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0102)
-	log.Infof("handle 0102 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0102)
+    log.Infof("handle 0102 %v", entity)
 
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0002(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0002)
-	log.Infof("handle 0002 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0002)
+    log.Infof("handle 0002 %v", entity)
 
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0808(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0808)
-	session.Protocol = 2 //2013
-	log.Infof("handle 0808 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0808)
+    session.Protocol = 2 //2013
+    log.Infof("handle 0808 %v", entity)
 }
 
 // 处理上报位置
 func handle0200(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0200)
-	log.Infof("handle 0200 %v", entity)
-	handleLocation(message.Header.Imei, entity)
+    entity := message.Body.(*jt808.T808_0x0200)
+    log.Infof("handle 0200 %v", entity)
+    handleLocation(message.Header.Imei, entity)
 
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0704(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0704)
-	log.Infof("handle 0704 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0704)
+    log.Infof("handle 0704 %v", entity)
 
-	for _, item := range entity.Items {
-		handleLocation(message.Header.Imei, &item)
-	}
+    for _, item := range entity.Items {
+        handleLocation(message.Header.Imei, &item)
+    }
 
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handleLocation(imei uint64, entity *jt808.T808_0x0200) {
-	fields := log.Fields{
-		"Imei": imei,
-		"警告": fmt.Sprintf("0x%x", entity.Alarm),
-		"状态": fmt.Sprintf("0x%x", entity.Status),
-		"纬度": entity.Lat,
-		"经度": entity.Lng,
-		"海拔": entity.Altitude,
-		"速度": entity.Speed,
-		"方向": entity.Direction,
-		"时间": entity.Time,
-	}
-	if entity.Status.Positioning() {
+    fields := log.Fields{
+        "Imei": imei,
+        "警告": fmt.Sprintf("0x%x", entity.Alarm),
+        "状态": fmt.Sprintf("0x%x", entity.Status),
+        "纬度": entity.Lat,
+        "经度": entity.Lng,
+        "海拔": entity.Altitude,
+        "速度": entity.Speed,
+        "方向": entity.Direction,
+        "时间": entity.Time,
+    }
+    if entity.Status.Positioning() {
+        date := entity.Time.Format("20060102")
+        iDate, _ := strconv.Atoi(date)
 
-	}
+        var iLat, iLng uint64
+        fLat, _ := entity.Lat.Float64()
+        fLng, _ := entity.Lng.Float64()
+        iLat = uint64(fLat * float64(1000000))
+        iLng = uint64(fLng * float64(1000000))
 
-	for _, ext := range entity.Extras {
-		switch ext.ID() {
-		case extra.Extra_0x01{}.ID():
-			fields["行驶里程"] = ext.(*extra.Extra_0x01).Value()
-		case extra.Extra_0x02{}.ID():
-			fields["剩余油量"] = ext.(*extra.Extra_0x02).Value()
-		}
-	}
+        loc := storage.Location{
+            Imei:      imei,
+            Date:      iDate,
+            Time:      entity.Time.Unix(),
+            Addr:      "",
+            Direction: entity.Direction,
+            Lat:       iLat,
+            Lng:       iLng,
+            Speed:     entity.Speed,
+            Type:      0,
+            Wgs:       "",
+        }
 
-	date := entity.Time.Format("20060102")
-	iDate, _ := strconv.Atoi(date)
+        err := storage.InsertLocation(loc)
+        if err != nil {
+            log.Warnf("insert location err %v", err)
+        }
+    } else {
+        getLbsLocation(entity)
+    }
 
-	var iLat, iLng uint64
-	fLat, _ := entity.Lat.Float64()
-	fLng, _ := entity.Lng.Float64()
-	iLat = uint64(fLat * float64(1000000))
-	iLng = uint64(fLng * float64(1000000))
+    for _, ext := range entity.Extras {
+        switch ext.ID() {
+        case extra.Extra_0x01{}.ID():
+            fields["行驶里程"] = ext.(*extra.Extra_0x01).Value()
+        case extra.Extra_0x02{}.ID():
+            fields["剩余油量"] = ext.(*extra.Extra_0x02).Value()
+        }
+    }
 
-	loc := storage.Location{
-		Imei:      imei,
-		Date:      iDate,
-		Time:      entity.Time.Unix(),
-		Addr:      "",
-		Direction: entity.Direction,
-		Lat:       iLat,
-		Lng:       iLng,
-		Speed:     entity.Speed,
-		Type:      0,
-		Wgs:       "",
-	}
-
-	err := storage.InsertLocation(loc)
-	if err != nil {
-		log.Warnf("insert location err %v", err)
-	}
-
-	log.WithFields(fields).Info("上报终端位置信息")
+    log.WithFields(fields).Info("上报终端位置信息")
 }
 
 func handle1007(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x1007)
-	log.Infof("handle 1007 %v", entity)
+    entity := message.Body.(*jt808.T808_0x1007)
+    log.Infof("handle 1007 %v", entity)
 
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle1107(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x1107)
-	log.Infof("handle 1107 %v", entity)
+    entity := message.Body.(*jt808.T808_0x1107)
+    log.Infof("handle 1107 %v", entity)
 
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle1300(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x1300)
-	log.Infof("handle 1300 %v", entity)
+    entity := message.Body.(*jt808.T808_0x1300)
+    log.Infof("handle 1300 %v", entity)
 }
 
 func handle0116(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0116)
-	session.UserData["short_record"] = ShortRecord{
-		Imei:      session.ID(),
-		Writer:    common.NewWriter(),
-		StartTime: time.Now(),
-		Schedule:  0.0,
-	}
-	log.Infof("handle 0116 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0116)
+    session.UserData["short_record"] = ShortRecord{
+        Imei:      session.ID(),
+        Writer:    common.NewWriter(),
+        StartTime: time.Now(),
+        Schedule:  0.0,
+    }
+    log.Infof("handle 0116 %v", entity)
 }
 
 func handle0117(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0117)
+    entity := message.Body.(*jt808.T808_0x0117)
 
-	/*
-	   这里Encode的时候，会把packet的内容写走
-	   log.Infof("body is %v", entity)
-	   d, _ := message.Encode()
-	   log.Infof("0117 raw msg %x", common.GetHex(d))
-	   bdata, _ := entity.Encode()
-	   log.Infof("0117 raw body %x", common.GetHex(bdata))
-	*/
+    /*
+       这里Encode的时候，会把packet的内容写走
+       log.Infof("body is %v", entity)
+       d, _ := message.Encode()
+       log.Infof("0117 raw msg %x", common.GetHex(d))
+       bdata, _ := entity.Encode()
+       log.Infof("0117 raw body %x", common.GetHex(bdata))
+    */
 
-	shortRecord, ok := session.UserData["short_record"].(ShortRecord)
-	if ok {
-		shortRecord.Schedule = float32(entity.PkgNo) * 100.0 / float32(entity.PkgSize)
-		log.Infof("schedule is %.2f", shortRecord.Schedule)
-		buffer, _ := ioutil.ReadAll(entity.Packet)
-		shortRecord.Writer.Write(buffer)
-	}
+    shortRecord, ok := session.UserData["short_record"].(ShortRecord)
+    if ok {
+        shortRecord.Schedule = float32(entity.PkgNo) * 100.0 / float32(entity.PkgSize)
+        log.Infof("schedule is %.2f", shortRecord.Schedule)
+        buffer, _ := ioutil.ReadAll(entity.Packet)
+        shortRecord.Writer.Write(buffer)
+    }
 
-	if entity.PkgNo == entity.PkgSize {
-		fileSize := len(shortRecord.Writer.Bytes())
-		duration := calDuration(fileSize)
-		fileName := fmt.Sprintf("%v_%v.amr", shortRecord.Imei, shortRecord.StartTime.Unix())
-		reader := bytes.NewReader(shortRecord.Writer.Bytes())
-		err := storage.UploadFile("record", fileName, reader, int64(fileSize))
-		if err != nil {
-			log.Warnf("upload record failed %v", err)
-		}
+    if entity.PkgNo == entity.PkgSize {
+        fileSize := len(shortRecord.Writer.Bytes())
+        duration := calDuration(fileSize)
+        fileName := fmt.Sprintf("%v_%v.amr", shortRecord.Imei, shortRecord.StartTime.Unix())
+        reader := bytes.NewReader(shortRecord.Writer.Bytes())
+        err := storage.UploadFile("record", fileName, reader, int64(fileSize))
+        if err != nil {
+            log.Warnf("upload record failed %v", err)
+        }
 
-		record := storage.Record{
-			Imei:     shortRecord.Imei,
-			Time:     shortRecord.StartTime.Unix(),
-			Duration: duration,
-			Filename: fileName,
-			Status:   0,
-		}
-		err = storage.InsertRecord(record)
-		if err != nil {
-			log.Warnf("insert record failed %v", err)
-		}
-		shortRecord.Writer.Reset()
-	}
+        record := storage.Record{
+            Imei:     shortRecord.Imei,
+            Time:     shortRecord.StartTime.Unix(),
+            Duration: duration,
+            Filename: fileName,
+            Status:   0,
+        }
+        err = storage.InsertRecord(record)
+        if err != nil {
+            log.Warnf("insert record failed %v", err)
+        }
+        shortRecord.Writer.Reset()
+    }
 
-	data, _ := message.Encode()
-	log.Infof("0117 raw msg %x", common.GetHex(data))
-	session.ReplyShortRecord(entity.PkgNo)
+    data, _ := message.Encode()
+    log.Infof("0117 raw msg %x", common.GetHex(data))
+    session.ReplyShortRecord(entity.PkgNo)
 }
 
 func handle0109(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0109)
-	log.Infof("handle 0109 %v", entity)
-	session.ReplyTime()
+    entity := message.Body.(*jt808.T808_0x0109)
+    log.Infof("handle 0109 %v", entity)
+    session.ReplyTime()
 }
 
 func handle0003(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0003)
-	log.Infof("handle 0109 %v", entity)
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    entity := message.Body.(*jt808.T808_0x0003)
+    log.Infof("handle 0109 %v", entity)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0105(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0105)
-	session.Reply8125()
-	log.Infof("handle 0105 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0105)
+    session.Reply8125()
+    log.Infof("handle 0105 %v", entity)
 }
 
 func handle0108(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0108)
-	log.Infof("handle 0108 %v", entity)
-	session.Reply8108()
+    entity := message.Body.(*jt808.T808_0x0108)
+    log.Infof("handle 0108 %v", entity)
+    session.Reply8108()
 }
 
 func handle0210(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0210)
-	log.Infof("handle 0210 %v", entity)
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    entity := message.Body.(*jt808.T808_0x0210)
+    log.Infof("handle 0210 %v", entity)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0115(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0115)
-	log.Infof("handle 0115 %v", entity)
-	delete(session.UserData, "short_record")
-	//session.Reply8115(entity.SessionId)
+    entity := message.Body.(*jt808.T808_0x0115)
+    log.Infof("handle 0115 %v", entity)
+    delete(session.UserData, "short_record")
+    //session.Reply8115(entity.SessionId)
 }
 
 func handle0120(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0120)
-	log.Infof("handle 0120 %v", entity)
-	session.UserData["vor_record"] = &VorRecord{
-		Imei:        session.ID(),
-		Writer:      common.NewWriter(),
-		StartTime:   time.Now(),
-		EndTime:     time.Now(),
-		FirstPacket: true,
-		PkgCnt:      0,
-	}
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    entity := message.Body.(*jt808.T808_0x0120)
+    log.Infof("handle 0120 %v", entity)
+    session.UserData["vor_record"] = &VorRecord{
+        Imei:        session.ID(),
+        Writer:      common.NewWriter(),
+        StartTime:   time.Now(),
+        EndTime:     time.Now(),
+        FirstPacket: true,
+        PkgCnt:      0,
+    }
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0118(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0118)
+    entity := message.Body.(*jt808.T808_0x0118)
 
-	currBeginTime := entity.Time
+    currBeginTime := entity.Time
 
-	vorRecord, ok := session.UserData["vor_record"].(*VorRecord)
-	if !ok {
-		session.UserData["vor_record"] = &VorRecord{
-			Imei:        session.ID(),
-			Writer:      common.NewWriter(),
-			StartTime:   time.Now(),
-			EndTime:     time.Now(),
-			FirstPacket: true,
-			PkgCnt:      0,
-		}
-		vorRecord, _ = session.UserData["vor_record"].(*VorRecord)
-	}
+    vorRecord, ok := session.UserData["vor_record"].(*VorRecord)
+    if !ok {
+        session.UserData["vor_record"] = &VorRecord{
+            Imei:        session.ID(),
+            Writer:      common.NewWriter(),
+            StartTime:   time.Now(),
+            EndTime:     time.Now(),
+            FirstPacket: true,
+            PkgCnt:      0,
+        }
+        vorRecord, _ = session.UserData["vor_record"].(*VorRecord)
+    }
 
-	if vorRecord.FirstPacket {
-		vorRecord.StartTime = entity.Time
-		vorRecord.EndTime = vorRecord.StartTime.Add(time.Second * 10)
-		vorRecord.FirstPacket = false
-	}
-	if entity.PkgNo == 1 { //组包
-		if currBeginTime.Sub(vorRecord.EndTime).Seconds() < 3 &&
-			currBeginTime.Sub(vorRecord.StartTime).Seconds() < 59 &&
-			vorRecord.PkgCnt < 47 {
-			vorRecord.EndTime = currBeginTime.Add(time.Second * 10)
-			buffer, err := ioutil.ReadAll(entity.Packet)
-			log.Infof("rayjay buffer len %v, err is %v", len(buffer), err)
-			vorRecord.Writer.Write(buffer)
-			vorRecord.PkgCnt += 1
-		} else { //上报当前缓存录音
-			fileName := fmt.Sprintf("%v_%v.amr", vorRecord.Imei, vorRecord.StartTime.Unix())
-			fileSize := len(vorRecord.Writer.Bytes())
-			duration := calDuration(fileSize)
-			reader := bytes.NewReader(vorRecord.Writer.Bytes())
-			err := storage.UploadFile("record", fileName, reader, int64(fileSize))
-			if err != nil {
-				log.Warnf("upload record failed %v", err)
-			}
-			record := storage.Record{
-				Imei:     vorRecord.Imei,
-				Time:     vorRecord.StartTime.Unix(),
-				Duration: duration,
-				Filename: fileName,
-				Status:   0,
-			}
-			err = storage.InsertRecord(record)
-			if err != nil {
-				log.Warnf("insert record failed %v", err)
-			}
-			vorRecord.Writer.Reset()
+    if vorRecord.FirstPacket {
+        vorRecord.StartTime = entity.Time
+        vorRecord.EndTime = vorRecord.StartTime.Add(time.Second * 10)
+        vorRecord.FirstPacket = false
+    }
+    if entity.PkgNo == 1 { //组包
+        if currBeginTime.Sub(vorRecord.EndTime).Seconds() < 3 &&
+            currBeginTime.Sub(vorRecord.StartTime).Seconds() < 59 &&
+            vorRecord.PkgCnt < 47 {
+            vorRecord.EndTime = currBeginTime.Add(time.Second * 10)
+            buffer, err := ioutil.ReadAll(entity.Packet)
+            log.Infof("rayjay buffer len %v, err is %v", len(buffer), err)
+            vorRecord.Writer.Write(buffer)
+            vorRecord.PkgCnt += 1
+        } else { //上报当前缓存录音
+            fileName := fmt.Sprintf("%v_%v.amr", vorRecord.Imei, vorRecord.StartTime.Unix())
+            fileSize := len(vorRecord.Writer.Bytes())
+            duration := calDuration(fileSize)
+            reader := bytes.NewReader(vorRecord.Writer.Bytes())
+            err := storage.UploadFile("record", fileName, reader, int64(fileSize))
+            if err != nil {
+                log.Warnf("upload record failed %v", err)
+            }
+            record := storage.Record{
+                Imei:     vorRecord.Imei,
+                Time:     vorRecord.StartTime.Unix(),
+                Duration: duration,
+                Filename: fileName,
+                Status:   0,
+            }
+            err = storage.InsertRecord(record)
+            if err != nil {
+                log.Warnf("insert record failed %v", err)
+            }
+            vorRecord.Writer.Reset()
 
-			//重新初始化并写入第一个包
-			vorRecord.PkgCnt = 0
-			vorRecord.FirstPacket = true
-			buffer, _ := ioutil.ReadAll(entity.Packet)
-			vorRecord.Writer.Write(buffer)
-			vorRecord.PkgCnt += 1
-		}
-	} else {
-		buffer, _ := ioutil.ReadAll(entity.Packet)
-		vorRecord.Writer.Write(buffer)
-		vorRecord.PkgCnt += 1
-	}
-	log.Infof("vor %v", *vorRecord)
+            //重新初始化并写入第一个包
+            vorRecord.PkgCnt = 0
+            vorRecord.FirstPacket = true
+            buffer, _ := ioutil.ReadAll(entity.Packet)
+            vorRecord.Writer.Write(buffer)
+            vorRecord.PkgCnt += 1
+        }
+    } else {
+        buffer, _ := ioutil.ReadAll(entity.Packet)
+        vorRecord.Writer.Write(buffer)
+        vorRecord.PkgCnt += 1
+    }
+    log.Infof("vor %v", *vorRecord)
 
-	session.ReplyVorRecord(entity)
+    session.ReplyVorRecord(entity)
 }
 
 func handle0119(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0119)
-	log.Infof("handle 0119 %v", entity)
-	session.Reply(message, jt808.T808_0x8100_ResultSuccess)
+    entity := message.Body.(*jt808.T808_0x0119)
+    log.Infof("handle 0119 %v", entity)
+    session.Reply(message, jt808.T808_0x8100_ResultSuccess)
 }
 
 func handle0001(session *server.Session, message *jt808.Message) {
-	entity := message.Body.(*jt808.T808_0x0001)
-	log.Infof("handle 0001 %v", entity)
+    entity := message.Body.(*jt808.T808_0x0001)
+    log.Infof("handle 0001 %v", entity)
 }
 
 func handleLbs(entity jt808.T808_0x0200) {
--- app/jt808_server/util.go
+++ app/jt808_server/util.go
@@ -1,12 +1,248 @@
 package main
 
-func calDuration(fileSize int) int {
-	quotient := fileSize / 702
-	remainder := fileSize % 702
+import (
+    "bytes"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "github.com/rayjay214/parser/common"
+    "github.com/rayjay214/parser/jt808"
+    "github.com/rayjay214/parser/jt808/extra"
+    log "github.com/sirupsen/logrus"
+    "io/ioutil"
+    "net/http"
+    "strings"
+)
 
-	// 如果余数大于等于除数的一半，向上取整
-	if remainder >= 702/2 {
-		quotient++
-	}
-	return quotient
+type Bts struct {
+    Lac    uint16
+    Cellid uint32
+    Rssi   uint8
+}
+
+type LbsInfo struct {
+    Mcc     uint16
+    Mnc     uint8
+    BtsList []Bts
+}
+
+type Mac struct {
+    MacAddr string
+    Rssi    uint8
+}
+
+type WifiInfo struct {
+    MacList []Mac
+}
+
+func getLbsLocation(entity *jt808.T808_0x0200) error {
+    log.Warnf("enter getlbs")
+    url := "http://114.215.191.234/locapi"
+
+    var lbsInfo LbsInfo
+    var wifiInfo WifiInfo
+
+    fParseE1 := func(lbsContent []byte, info *LbsInfo) error {
+        reader := common.NewReader(lbsContent)
+
+        var err error
+        info.Mcc, err = reader.ReadUint16()
+        if err != nil {
+            return err
+        }
+        Mnc, err := reader.ReadUint16()
+        if err != nil {
+            return err
+        }
+        info.Mnc = uint8(Mnc)
+
+        btsNum := (len(lbsContent) - 4) / 8
+        for i := 0; i < btsNum; i++ {
+            var bts Bts
+            reader.ReadByte() //useless byte
+            bts.Lac, err = reader.ReadUint16()
+            if err != nil {
+                return err
+            }
+            bts.Cellid, err = reader.ReadUint32()
+            if err != nil {
+                return err
+            }
+            rssi, err := reader.ReadByte()
+            if err != nil {
+                return err
+            }
+            bts.Rssi = rssi*2 - 113
+            info.BtsList = append(info.BtsList, bts)
+        }
+        return nil
+    }
+
+    fParseEB := func(lbsContent []byte, info *LbsInfo) error {
+        reader := common.NewReader(lbsContent)
+
+        var err error
+        info.Mcc, err = reader.ReadUint16()
+        if err != nil {
+            return err
+        }
+        info.Mnc, err = reader.ReadByte()
+        if err != nil {
+            return err
+        }
+
+        btsNum := (len(lbsContent) - 3) / 5
+        for i := 0; i < btsNum; i++ {
+            var bts Bts
+            bts.Lac, err = reader.ReadUint16()
+            if err != nil {
+                return err
+            }
+            cellid, err := reader.ReadUint16()
+            if err != nil {
+                return err
+            }
+            bts.Cellid = uint32(cellid)
+            rssi, err := reader.ReadByte()
+            if err != nil {
+                return err
+            }
+            bts.Rssi = rssi*2 - 113
+            info.BtsList = append(info.BtsList, bts)
+        }
+        return nil
+    }
+
+    fParseEE := func(lbsContent []byte, info *LbsInfo) error {
+        reader := common.NewReader(lbsContent)
+
+        var err error
+        info.Mcc, err = reader.ReadUint16()
+        if err != nil {
+            return err
+        }
+        info.Mnc, err = reader.ReadByte()
+        if err != nil {
+            return err
+        }
+
+        var bts Bts
+
+        bts.Lac, err = reader.ReadUint16()
+        if err != nil {
+            return err
+        }
+        bts.Cellid, err = reader.ReadUint32()
+        if err != nil {
+            return err
+        }
+        rssi, err := reader.ReadByte()
+        if err != nil {
+            return err
+        }
+        bts.Rssi = rssi*2 - 113
+        info.BtsList = append(info.BtsList, bts)
+
+        return nil
+    }
+
+    fParseEC := func(wifiContent []byte, info *WifiInfo) error {
+        reader := common.NewReader(wifiContent)
+
+        wifiNum := len(wifiContent) / 7
+
+        var err error
+        for i := 0; i < wifiNum; i++ {
+            var mac Mac
+            mac.MacAddr, err = reader.ReadString(6)
+            if err != nil {
+                return err
+            }
+            mac.Rssi, err = reader.ReadByte()
+            if err != nil {
+                return err
+            }
+            info.MacList = append(info.MacList, mac)
+        }
+        return nil
+    }
+
+    for _, ext := range entity.Extras {
+        switch ext.ID() {
+        case extra.Extra_0xeb{}.ID():
+            lbsContent := ext.(*extra.Extra_0xeb).Data()
+            fParseEB(lbsContent, &lbsInfo)
+        case extra.Extra_0xe1{}.ID():
+            lbsContent := ext.(*extra.Extra_0xe1).Data()
+            fParseE1(lbsContent, &lbsInfo)
+        case extra.Extra_0xee{}.ID():
+            lbsContent := ext.(*extra.Extra_0xee).Data()
+            fParseEE(lbsContent, &lbsInfo)
+        case extra.Extra_0xec{}.ID():
+            wifiContent := ext.(*extra.Extra_0xec).Data()
+            fParseEC(wifiContent, &wifiInfo)
+        }
+    }
+
+    if len(wifiInfo.MacList) == 0 && len(lbsInfo.BtsList) == 0 {
+        return errors.New("no valid lbs or wifi info")
+    }
+
+    body := make(map[string]string)
+
+    if len(lbsInfo.BtsList) > 0 {
+        body["accesstype"] = "0"
+        body["bts"] = fmt.Sprintf("%d,%d,%d,%d,%d", lbsInfo.Mcc, lbsInfo.Mnc, lbsInfo.BtsList[0].Lac, lbsInfo.BtsList[0].Cellid, lbsInfo.BtsList[0].Rssi)
+        var btsList []string
+        for _, bts := range lbsInfo.BtsList {
+            strBts := fmt.Sprintf("%d,%d,%d,%d,%d", lbsInfo.Mcc, lbsInfo.Mnc, bts.Lac, bts.Cellid, bts.Rssi)
+            btsList = append(btsList, strBts)
+        }
+        body["nearbts"] = strings.Join(btsList, "|")
+    }
+
+    if len(wifiInfo.MacList) > 0 {
+        body["accesstype"] = "1"
+        var macList []string
+        for _, mac := range wifiInfo.MacList {
+            strMac := fmt.Sprintf("%s,%d", mac.MacAddr, mac.Rssi)
+            macList = append(macList, strMac)
+        }
+        body["macs"] = strings.Join(macList, ",")
+    }
+    byteData, _ := json.Marshal(body)
+    reader := bytes.NewReader(byteData)
+
+    request, err := http.NewRequest("POST", url, reader)
+    defer request.Body.Close()
+    if err != nil {
+        return err
+    }
+    request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+
+    client := http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        return err
+    }
+
+    respBytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return err
+    }
+    log.Infof("resp is %v", string(respBytes))
+
+    return nil
+
+}
+
+func calDuration(fileSize int) int {
+    quotient := fileSize / 702
+    remainder := fileSize % 702
+
+    // 如果余数大于等于除数的一半，向上取整
+    if remainder >= 702/2 {
+        quotient++
+    }
+    return quotient
 }
